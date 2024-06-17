ac545ea52c3cc9cd92f150b14b5c2437554e6e01
eseguro <estefannyseguro.777@gmail.com>
2024-06-17 16:04:46 -0500

    17 - Custom actions Javascript 02

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,52 @@
 const core = require('@actions/core');
- 
-async function run() { 
-  core.info('I am a custom JS action');
+const exec = require('@actions/exec');
+
+function validateInput(input, regex) {
+  // Use the provided regex for validation
+  return regex.test(input);
 }
- 
+const branchRegex = /^[a-zA-Z0-9_-\/\.]+$/;
+const pathRegex = /^[a-zA-Z0-9_-\/]+$/;
+
+async function run() {
+  const baseBranch = core.getInput('base-branch', { required: false });
+  const targetBranch = core.getInput('target-branch', { required: false });
+  const workingDirectory = core.getInput('working-directory', { required: true });
+  const debug = core.getBooleanInput('debug', { required: false });
+  const ghToken = core.getInput('gh-token');
+
+  core.setSecret(ghToken);
+
+  if (!validateInput(baseBranch, branchRegex))
+    core.setFailed(`Invalid base-branch ${baseBranch}`);
+
+  if (!validateInput(targetBranch, branchRegex))
+    core.setFailed(`Invalid target-branch ${targetBranch}`);
+
+  if (!validateInput(workingDirectory, pathRegex))
+    core.setFailed(`Invalid working-directory ${workingDirectory}`);
+
+  core.info(`base-branch ${baseBranch}`);
+  core.info(`target-branch ${targetBranch}`);
+  core.info(`working-directory ${workingDirectory}`);
+
+  await exec.exec('npm update', [], {
+    cwd: workingDir,
+  });
+
+  const gitStatus = await exec.getExecOutput(
+    'git status -s package*.json',
+    [],
+    {
+      cwd: workingDir,
+    }
+  );
+
+  if (gitStatus.stdout.length > 0)
+    core.info('[js-dependency-update] : There are updates available!');
+  else
+    core.info('[js-dependency-update] : No updates at this point in time.');
+
+}
+
 run();
